33ce6c6b38205cc72855e8d9b11de149394b67b9
Mariarti <mariarti91@gmail.com>
2013-09-14 18:57:30 +0700

    added hiders on initTasks for linux syslogs

--- project/computer_examination/src/coex/coex_initTasks.cpp
+++ project/computer_examination/src/coex/coex_initTasks.cpp
@@ -3,6 +3,7 @@
 #include "../tasks/example_task.h"
 #include "../tasks/task_search_pidgin_win.h"
 #include "../tasks/task_search_programm_win.h"
+#include "../tasks/task_search_syslogs_lin.h"
 
 namespace coex {
   void initTasks(const coex::typeOS &os, QVector<coex::task*> &tasks)
@@ -10,5 +11,6 @@ namespace coex {
     coex::createTask<exampleTask>(os, tasks);
     coex::createTask<taskSearchPidginWin>(os, tasks);
     coex::createTask<taskSearchProgrammWin>(os, tasks);
+    coex::createTask<taskSearchSyslogsLin>(os, tasks);
   };
 };
