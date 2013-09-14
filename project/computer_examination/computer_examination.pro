3c03024cd37959b90fd9265bda8d93ba0086bd5b
Mariarti <mariarti91@gmail.com>
2013-09-14 18:45:49 +0700

    add task for searching logs on linux

--- project/computer_examination/computer_examination.pro
+++ project/computer_examination/computer_examination.pro
@@ -5,13 +5,15 @@ SOURCES += src/main.cpp \
   src/coex/coex_initTasks.cpp \
   src/tasks/example_task.cpp \
   src/tasks/task_search_pidgin_win.cpp \
-  src/tasks/task_search_programm_win.cpp
+  src/tasks/task_search_programm_win.cpp \
+  src/tasks/task_search_syslogs_lin.cpp
 
 HEADERS += src/coex/task.h \
   src/coex/coex.h \
   src/tasks/example_task.h \
   src/tasks/task_search_pidgin_win.h \
-  src/tasks/task_search_programm_win.h
+  src/tasks/task_search_programm_win.h \
+  src/tasks/task_search_syslogs_lin.h
 QT += core \
  xml \
  script \
---
+++ project/computer_examination/src/tasks/task_search_syslogs_lin.cpp
@@ -0,0 +1,38 @@
+#include "task_search_syslogs_lin.h"
+#include <iostream>
+
+taskSearchSyslogsLin::taskSearchSyslogsLin()
+{
+	m_strName = "taskSearchSyslogsLin";
+	m_strDescription = " It 's example task";
+};
+
+bool taskSearchSyslogsLin::supportOS(const coex::typeOS &os)
+{
+	if(os == coex::ceLinux)
+		return true;
+	return false;
+};
+
+QString taskSearchSyslogsLin::name()
+{
+	return m_strName;
+};
+
+QString taskSearchSyslogsLin::description()
+{
+	return m_strDescription;
+};
+
+bool taskSearchSyslogsLin::test()
+{
+	// unit test
+	return true;
+};
+
+bool taskSearchSyslogsLin::execute(const coex::config &config)
+{
+	std::cout << ">>>>>> same execute...\n\n";
+	return true;
+};
+		
