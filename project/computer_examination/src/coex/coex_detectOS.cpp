a9ae7bc4cfdf66ee5c8ac172ecf66903e96b6382
Eugene Sopov <eso@startext.tomsk.ru>
2013-09-27 10:53:10 +0700

    changed detect OS

--- project/computer_examination/src/coex/coex_detectOS.cpp
+++ project/computer_examination/src/coex/coex_detectOS.cpp
@@ -1,10 +1,28 @@
 #include "coex.h"
 
+#include <QFile>
+#include <QFileInfo>
+#include <QCryptographicHash>
+#include <QIODevice>
+
 namespace coex {
-  coex::typeOS detectOS(QString /*inputFolder*/)
+  coex::typeOS detectOS(QString inputFolder)
   {
-    // TODO: detect system
-    // inputFolder
+    QFileInfo fi(inputFolder + "//Windows//System32//kernel32.dll");
+
+    if(fi.exists())
+    {
+      QFile file(fi.fullPath());
+      if (file.open(QIODevice::ReadOnly)) 
+      {
+        QByteArray fileData = file.readAll();
+        QByteArray hashData = QCryptographicHash::hash(fileData, QCryptographicHash::Md5);
+        QString str = hashData.toHex();
+
+        if(str == "941b22338052e8a0ada7944b827e3e2a") // 6.2.9200.16627
+         return coex::ceWindows8;
+    };
+
     return coex::ceWindowsXP;  
   };  
 };
