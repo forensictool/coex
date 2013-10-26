71263a7f2176a9f5534971f6bd23db675fb5626f
sea-kg <mrseakg@gamail.com>
2013-10-27 01:57:35 +0700

    fixed insert pictures to report pdf

--- docs/tex/make_report.sh
+++ docs/tex/make_report.sh
@@ -1,15 +1,23 @@
 # cp -R gpo.pdf gpo_backup.pdf
 rm gpo.pdf
-# convert images-arch/alg_main.png images-arch/alg_main.eps
+# find . -type f -name "*.png" -printf "convert %p && sh %p && rm %p \n" | sh
+convert images-arch/alg_main.png images-arch/alg_main.eps
 # sam2p images-arch/alg_main.png images-arch/alg_main.eps
 
-pdflatex gpo.tex
+latex gpo.tex
+# повторяем что бы сгенерировалось Оглавление
+latex gpo.tex
+# создаем pdf
+dvipdf gpo.dvi 
+
+
 
 # еще раз что бы оглавление встало как надо
-pdflatex gpo.tex
+# pdflatex gpo.tex
 
 rm gpo.log
 rm gpo.toc
 rm gpo.aux
+rm gpo.dvi
 
 
