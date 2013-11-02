b2fdbba50a62296943e09ac8bca5d2a68100a9c4
sea-kg <mrseakg@gamail.com>
2013-11-03 04:00:31 +0700

    added implementation for read from win events file *.Evt

--- thirdparty/parse/main.cpp
+++ thirdparty/parse/main.cpp
@@ -3,9 +3,12 @@
 #include <QStringList>
 #include <QString>
 #include <QFile>
+#include <QStringList>
 #include <QDataStream>
 #include <QIODevice>
 #include <QVector>
+#include <QMap>
+#include <QDateTime>
 #include <QDirIterator>
 
 
@@ -49,6 +52,252 @@ DWORD DataOffset;
 }
 */
 
+// helpers function
+
+qint32 readQInt32asLittleEndian(QDataStream &stream)
+{
+	QDataStream::ByteOrder bo = stream.byteOrder();
+	stream.setByteOrder(QDataStream::LittleEndian);
+	quint32 res;
+	stream >> res;
+	stream.setByteOrder(bo);
+	return res;
+}
+
+qint16 readQInt16asLittleEndian(QDataStream &stream)
+{
+	QDataStream::ByteOrder bo = stream.byteOrder();
+	stream.setByteOrder(QDataStream::LittleEndian);
+	quint16 res;
+	stream >> res;
+	stream.setByteOrder(bo);
+	return res;
+}
+
+QString readString(QDataStream &stream)
+{
+	QString res = "";
+	while(true || !stream.atEnd())
+	{
+		char data[2];
+		stream.readRawData(data, 2);
+		
+		if (data[0] == 0x00)
+			break;
+	
+		if(data[0] != 0x00)
+			res += QChar::fromAscii(data[0]);
+	}
+	return res;
+}
+
+QString EventTypeToString(quint16 type)
+{
+	if((type & 0x0001) == 0x0001) return "Error event";
+	if((type & 0x0010) == 0x0010) return "Failure Audit event";
+	if((type & 0x0008) == 0x0008) return "Success Audit event";
+	if((type & 0x0004) == 0x0004) return "Information event";
+	if((type & 0x0002) == 0x0002) return "Warning event";
+	return "";
+}
+			
+QString EventCategoryToString(quint16 type)
+{
+	// TODO:
+	if(type == 0) return "None";
+	return "Unknown";
+}
+
+class _EVENTLOGRECORD 
+{
+	public:
+		_EVENTLOGRECORD() {};
+		~_EVENTLOGRECORD() 
+		{
+				// delete Bytes;
+		};
+		
+		char *Bytes;
+		
+		quint32 Length;
+		quint32 Reserved;
+		quint32 RecordNumber;
+		quint32 TimeGenerated;
+		quint32 TimeWritten;
+		quint32 EventID;
+		quint16 EventType;
+		quint16 NumStrings;
+		quint16 EventCategory;
+		quint16 ReservedFlags;
+		quint32 ClosingRecordNumber;
+		quint32 StringOffset;
+		quint32 UserSidLength;
+		quint32 UserSidOffset;
+		quint32 DataLength;
+		quint32 DataOffset;
+
+		QMap<QString, QString> MapData;
+	
+		// http://msdn.microsoft.com/en-us/library/windows/desktop/aa363651%28v=vs.85%29.aspx
+		
+		void setEventID(quint32 id)
+		{
+			// TODO
+			quint8 Sev = id >> 30;
+			
+			QString Severity = "";
+			if(Sev == 0x00) Severity = "Success";
+			if(Sev == 0x01) Severity = "Informational";
+			if(Sev == 0x02) Severity = "Warning";
+			if(Sev == 0x03) Severity = "Error";
+			MapData["EventID_SeverityToString"] = Severity;
+			MapData["EventID_Severity"] = QString::number(Sev);
+			
+			quint8 C = id >> 29;
+			C = C & 0x01;
+			MapData["EventID_CustomerBit"] = QString::number(C);
+			if(C == 0x00)
+				MapData["EventID_CustomerBitToString"] = "System code";
+			else 
+				MapData["EventID_CustomerBitToString"] = "Customer code";
+			
+			quint8 R = id >> 28;
+			R = R & 0x01;
+			MapData["EventID_ReservedBit"] = QString::number(R);
+			
+			quint16 F = id >> 16;
+			F = F & 0x0FFF;
+			MapData["EventID_Facility"] = QString::number(F);
+			
+			EventID = (id & 0x0000FFFF);
+			MapData["EventID_Code"] = QString::number(EventID);
+		}
+
+		void read(QDataStream &stream)
+		{
+			stream >> Reserved;				
+			if(Reserved != 0x4c664c65)
+				return;
+
+			MapData["Reserved"] = "0x4c664c65";
+
+			RecordNumber = readQInt32asLittleEndian(stream);
+			MapData["RecordNumber"] = QString::number(RecordNumber);
+
+			// 2 Date Created (4 bytes, little endian, decimal value in epoch)
+			TimeGenerated = readQInt32asLittleEndian(stream);
+			QDateTime dtTimeGenerated = QDateTime::fromTime_t(TimeGenerated);
+			MapData["TimeGenerated"] = QString::number(TimeGenerated);
+			MapData["TimeGeneratedToDate"] = dtTimeGenerated.toUTC().toString("yyyy-MM-dd");
+			MapData["TimeGeneratedToTime"] = dtTimeGenerated.toUTC().toString("hh:mm:ss");
+			MapData["TimeGeneratedToDateTime"] = dtTimeGenerated.toUTC().toString("yyyy-MM-dd hh:mm:ss");
+		
+			// 3 Date Written (4 bytes, little endian, decimal value in epoch)
+			TimeWritten = readQInt32asLittleEndian(stream);
+			QDateTime dtTimeWritten = QDateTime::fromTime_t(TimeWritten);
+			MapData["TimeWritten"] = QString::number(TimeWritten);
+			MapData["TimeWrittenToDate"] = dtTimeWritten.toUTC().toString("yyyy-MM-dd");
+			MapData["TimeWrittenToTime"] = dtTimeWritten.toUTC().toString("hh:mm:ss");
+			MapData["TimeWrittenToDateTime"] = dtTimeWritten.toUTC().toString("yyyy-MM-dd hh:mm:ss");
+			
+			// 4 Event ID (4 bytes, little endian, decimal value)
+			// !!! TODO 
+			EventID = readQInt32asLittleEndian(stream);
+			MapData["EventID"] = QString::number(EventID);
+			setEventID(EventID);
+
+			// 7 Event Type (2 byte - number - used as index to retrieve 'Event Name')
+			EventType = readQInt16asLittleEndian(stream);
+			MapData["EventType"] = QString::number(EventType);
+			MapData["EventTypeToString"] = EventTypeToString(EventType);
+			
+			// 8 String Count (2 bytes - The number of strings in the event in decimal)
+			// stream >> NumStrings;
+			NumStrings = readQInt16asLittleEndian(stream);
+			MapData["NumStrings"] = QString::number(NumStrings);
+			
+			// 9 Event Category (2 bytes - decimal value)
+			stream >> EventCategory;
+			MapData["EventCategory"] = QString::number(EventCategory);
+			MapData["EventCategoryToString"] = EventCategoryToString(EventCategory);
+
+			// ReservedFlags
+			stream >> ReservedFlags;
+			MapData["ReservedFlags"] = QString::number(ReservedFlags);
+			
+			// ClosingRecordNumber
+			stream >> ClosingRecordNumber;
+			MapData["ClosingRecordNumber"] = QString::number(ClosingRecordNumber);
+			
+			// StringOffset
+			// stream >> StringOffset;
+			StringOffset = readQInt32asLittleEndian(stream);
+			MapData["StringOffset"] = QString::number(StringOffset);
+			
+			// UserSidLength
+			UserSidLength = readQInt32asLittleEndian(stream);
+			MapData["UserSidLength"] = QString::number(UserSidLength);
+			if(UserSidLength == 0)
+				MapData["User"] = "N/A";
+			
+			
+			// UserSidOffset
+			UserSidOffset = readQInt32asLittleEndian(stream);
+			MapData["UserSidOffset"] = QString::number(UserSidOffset);
+			
+			// DataLength
+			DataLength = readQInt32asLittleEndian(stream);
+			MapData["DataLength"] = QString::number(DataLength);
+			
+			// DataOffset
+			DataOffset = readQInt32asLittleEndian(stream);
+			MapData["DataOffset"] = QString::number(DataOffset);
+			
+			if(NumStrings > 0)
+			{
+				QStringList list;
+				QString str = readString(stream);
+				
+				while(str.count() > 0)
+				{
+					list << str;
+					str = readString(stream);
+				}
+
+				int nSize = list.size();
+				MapData["NumStrings_Real"] = QString::number(nSize);
+				
+				if(nSize > 0) MapData["Source"] = list[0];
+				if(nSize > 1) MapData["Computer"] = list[1];
+				if(nSize > 2)
+				{
+					QString Description;
+					for(int i = 2; i < nSize; i++)
+					{
+						if(Description.count() > 0 && list[i].count() > 0)
+							Description += " ";
+						Description += list[i];
+					}
+					MapData["Description"] = Description + ".";
+				}
+			}
+
+			// 17 Unknown? (8 bytes)
+			// stream.readRawData(Unknown5, sizeof(Unknown5));
+		};
+	
+		void print()
+		{
+			std::cout << "Event: \r\n";
+			QMapIterator<QString, QString> i(MapData);
+			while (i.hasNext()) {
+				i.next();
+				std::cout << "\t" << i.key().toStdString() << " = " << i.value().toStdString() << "\r\n";
+			}
+		};
+};
+
+
 class winEventLog
 {
 	public:
@@ -60,178 +309,35 @@ class winEventLog
 		
 		~winEventLog()
 		{
+			m_evtlogs.clear();
 			if(m_bOpen) m_file->close();
 		}
-		
-		struct _EVENTLOGRECORD 
-		{
-			quint32 MessageNumber;
-			quint32 DateCreated;
-			quint32 DateWritten;
-			quint16 EventID;
-			quint8 Unknown1;
-			quint8 Unknown2;
-			quint8 EventType;
-			QString strEventType;
-			quint16 StringCount;
-			quint16 Category;
-			QString strCategory;
-			char SID[8];
-			char Unknown3[8];
-			char Unknown4[11];
-			QString Message;
-			QString Source;
-			QString ComputerName;
-			QString Message1;
-			
-			char Unknown5[8];
-			
-			QString readString(QDataStream &stream)
-			{
-				bool bEnd = false;
-				QString res = "";
-				while(!bEnd)
-				{
-					char data[4];
-					stream.readRawData(data, 4);
-					
-					if(
-						(data[0] == 0x00 && data[2] == 0x00)
-						|| (data[2] == 0x00)
-					)
-						bEnd = true;
-					else
-					{
-						if(data[0] != 0x00)
-							res += QChar::fromAscii(data[0]);
-						else 
-							res += " ";
-						
-						if(data[2] != 0x00)
-							res += QChar::fromAscii(data[2]);
-						else 
-							res += " ";
-					}
-				}
-				return res;
-			}
-			
-			void read(QDataStream &stream)
-			{
-				// 1 Message Number (4 bytes - Padded with null characters from left to right to fill in the full 4 bytes. It is in little endian byte order)
-				stream >> MessageNumber;
-				
-				// 2 Date Created (4 bytes, little endian, decimal value in epoch)
-				stream >> DateCreated;
-				
-				// 3 Date Written (4 bytes, little endian, decimal value in epoch)
-				stream >> DateWritten;
-				
-				// 4 Event ID (2 bytes, little endian, decimal value)
-				stream >> EventID;
-				
-				// 5 Unknown? (1 byte)
-				stream >> Unknown1;
-				
-				// 6 Unknown? (1 byte)
-				stream >> Unknown2;
 
-				// 7 Event Type (1 byte - number - used as index to retrieve 'Event Name')
-				stream >> EventType;
-				if(EventType == 4) strEventType = "Information";
-				
-				// 8 String Count (2 bytes - The number of strings in the event in decimal)
-				stream >> StringCount;
-				
-				// 9 Category (2 bytes - decimal value)
-				stream >> Category;
-				if(Category == 0) strCategory = "None";
-				
-				
-				// 10 SID? (8 bytes - possibly the decimal value of the SID)
-				stream.readRawData(SID, sizeof(SID));
-				
-				// 11 Unknown? (8 bytes - possibly related to the SID)
-				stream.readRawData(Unknown3, sizeof(Unknown3));
-				
-				// 12 Unknown? (11 bytes)
-				stream.readRawData(Unknown4, sizeof(Unknown4));
-				
-				// 13 Source Name (Variable Length in words (4 bytes) with at least the last two bytes being null and 0 or more bytes of null padding up to the length of a full word)
-				Source = readString(stream);
-				
-				// 14 Computer Name (Variable Length in words (4 bytes) with at least the last two bytes being null and 0 or more bytes of null padding up to the length of a full word)
-				ComputerName = readString(stream);
-				
-				// 15 String1 (Variable Length - Also known as the 'Message' - Variable Length in words (4 bytes) with at least the last two bytes being null and 0 or more bytes of null padding up to the length of a full word.)
-				Message = readString(stream);
-				
-				// 16 String'n' (Depending on the number of strings specified by
-				{
-					char *strn = new char[StringCount+1];
-					strn[StringCount] = 0x00;
-					stream.readRawData(strn, StringCount);
-					Message1 = QString(strn);
-				}
-				
-				// 17 Unknown? (8 bytes)
-				stream.readRawData(Unknown5, sizeof(Unknown5));
-			}
-			
-			void print()
-			{
-				
-				
-				std::cout << "/ Event \\____________________________________________________\r\n";
-				std::cout << "| Date: [" << DateWritten << "]\t Source: [" << Source.toStdString() << "] |\r\n";
-				std::cout << "| Time: [" << DateCreated << "]\t Category: (" << Category << ") " << strCategory.toStdString() << " |\r\n";
-				std::cout << "| Type: [" << (int)EventType << ") " << strEventType.toStdString() << "]\t EventID: [" << EventID << "] |\r\n";
-				
-				// MessageNumber
-				
-				
-				
-				std::cout << "| Unknown1: " << (int)Unknown1 << "; ";
-				std::cout << "Unknown2: " << (int)Unknown2 << "; ";
-				std::cout << "StringCount: " << StringCount << "; ";
-				std::cout << "Category: " << Category << "; ";
-				std::cout << "SID: " << SID << "; ";
-				std::cout << "Unknown3: " << Unknown3 << "; ";
-				std::cout << "Unknown4: " << Unknown4 << ";";
-				std::cout << "Message: " << Message.toStdString() << "; ";
-				std::cout << "ComputerName: " << ComputerName.toStdString() << "; ";
-				std::cout << "Message1: " << Message1.toStdString() << "; ";
-			
-				std::cout << "\r\n";
-			}
-		};
-		
 		void read()
 		{
 			if(!m_bOpen) return;
 			QDataStream stream(m_file);
-			
-			// I don't know what is it:
-			
-			int nOffset = 32;
-			char data[nOffset];
-			stream.readRawData(data, nOffset);
-
 			while(!stream.atEnd())
 			{
-				quint32 unknown;
-				stream >> unknown;
-
-				// 0 Message Separator (4 bytes - Separator is (1001100011001100100110001100101 - bin) or (4c664c65 - hex) or (LfLe - ascii))
-				if(unknown == 0x4c664c65) // !!! found separator
-				{
-					_EVENTLOGRECORD evnt;
-					evnt.read(stream);					
-					evnt.print();
-				}
+				_EVENTLOGRECORD evnt;
+				evnt.Length = readQInt32asLittleEndian(stream)-4;
+				evnt.MapData["Length"] = QString::number(evnt.Length);
+				
+				if(evnt.Length == 0xfffffffc || int(evnt.Length) < 0)
+					return;
+				
+				evnt.Bytes = new char[evnt.Length];
+				stream.readRawData(evnt.Bytes, evnt.Length);
+				QByteArray ba(evnt.Bytes, evnt.Length);
+				evnt.MapData["Data"] = ba.toBase64();
+				
+				QDataStream streamRecord(ba);
+				evnt.read(streamRecord);
+				evnt.print();
+				m_evtlogs.push_back(evnt);
 			}
 		}
-	
+
 	private:
 		QVector<_EVENTLOGRECORD> m_evtlogs;
 		QFile *m_file;
