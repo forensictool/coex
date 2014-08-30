721d598c9d86357446818f585b929ec6d39eb29c
sea-kg <mrseakg@gamail.com>
2014-08-31 01:18:17 +0700

    defined interfaces

--- project2_so/include/coex.h
+++ project2_so/include/coex.h
@@ -6,6 +6,26 @@
 
 namespace coex
 {
+	class ITypeOperationSystem {
+		virtual QString platform() = 0;
+		/* example returned: "windows" or "linux" */
+		
+		virtual QString version() = 0;
+		/* example returned:
+		 * if platform is "windows" then "95" or "98" or "XP" or "Vista" or "7" or "8"
+		 * */
+		
+		virtual QString toString() = 0;
+		/*
+		 *
+		 * */
+
+		virtual bool isUnknown() = 0;
+		virtual bool isWindows() = 0;
+		virtual bool isLinux() = 0;
+		virtual void copyTo(coex::ITypeOperationSystem *) = 0;
+	};
+	
 	enum typeOS
 	{
 		ceUnknown,
@@ -17,6 +37,43 @@ namespace coex
 		ceWindows7,
 		ceWindows8
 	};
+
+	/* interface for
+	 * config object 
+	 * */
+	class IConfig {
+		public:
+			virtual QString inputFolder() = 0;
+			virtual QString outputFolder() = 0;
+			virtual coex::ITypeOperationSystem* typeOS() = 0;
+			
+			virtual void setInputFolder(QString path) = 0;
+			virtual void setOutputFolder(QString path) = 0;
+			virtual void setTypeOS(coex::ITypeOperationSystem*) = 0;
+	};
+	
+	/* interface for
+	 * detect operation system 
+	 * must be TODO */
+	class IDetectOperationSystem {
+		public:
+			virtual coex::ITypeOperationSystem* detect() = 0;
+			virtual QString author() = 0;
+	};
+
+	class ITask
+	{
+		public:
+			virtual QString manual() = 0;
+			virtual void setOption(QStringList) = 0;
+			virtual QString command() = 0;
+			virtual bool isSupportOS(const coex::ITypeOperationSystem *os) = 0;
+			virtual QString name() = 0;
+			virtual QString description() = 0;
+			virtual bool test() = 0;
+			virtual bool execute(const coex::IConfig *config) = 0;
+	};
+	
 
 	struct config
 	{
