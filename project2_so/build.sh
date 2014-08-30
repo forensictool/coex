96e0fbf5189fb79a36c582697afa4ff42a25db2b
sea-kg <mrseakg@gamail.com>
2014-08-31 02:10:55 +0700

    moved libs -> plugins and changed build.sh

--- project2_so/build.sh
+++ project2_so/build.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
+CURRPWD=$(echo "`pwd`")
+OK=" ..... ok"
+LOGFILE="$CURRPWD/log_build.log"
+ERRORLOGFILE="$CURRPWD/errorlog_build.log"
+echo $CURRPWD > $CURRPWD/log_build.log
 
+echo -ne " * delete all 'Makefile'"
 find . -iname 'Makefile' -delete
+echo $OK
+
+echo -ne " * delete all '*.o'"
 find . -iname '*.o' -delete
+echo $OK
 
-cd apps/coex/obj
-qmake-qt4 && make
+echo -ne " * delete all 'tmp'"
+find . -iname 'tmp' -delete
+echo $OK
 
-cd ../../../libs/detectOS/obj
-qmake-qt4 && make
+echo -ne " * build 'coex'"
+cd $CURRPWD/app/
+qmake-qt4
+make >> $LOGFILE 2>> $ERRORLOGFILE
+echo $OK
 
-cd ../../winSysLog/obj
-qmake-qt4 && make
+echo " * build plugins"
+cd $CURRPWD/plugins
+for D in `find ./ -mindepth 1 -maxdepth 1 -type d `
+do
+	cd $CURRPWD/plugins
+	cd $D
+	DIRNAME=$(basename "`pwd`")
+	echo -ne " * * build '$DIRNAME'"
+	
+	qmake-qt4 >> $LOGFILE 2>> $ERRORLOGFILE
+	if [ $? -ne 0 ]; then
+		echo ""
+		echo "ERRORS, log look in file: $ERRORLOGFILE"
+		exit;
+	fi
+
+	make >> $LOGFILE 2>> $ERRORLOGFILE
+	if [ $? -ne 0 ]; then
+		echo ""
+		echo "ERRORS, log look in file: $ERRORLOGFILE"
+		exit;
+	fi
+	echo $OK
+done
 
