776b1382b183619c92ef74895664185fcea73abb
sea-kg <mrseakg@gamail.com>
2014-08-31 22:09:06 +0700

    updated help

--- sources/app/src/helpers.cpp
+++ sources/app/src/helpers.cpp
@@ -13,6 +13,7 @@ void printHelp(int argc, char* argv[]) {
 	QString nameProgram(argv[0]);
 	std::cout << "\nUsage " << nameProgram.toStdString() << " <inputFolder> <outputFolder> \n";
 	std::cout << "\nor Usage --listdetectos \n";
+	std::cout << "\nor Usage --listtaskos \n";
 };
 
 
