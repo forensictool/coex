d46a6671f917fba1abe17b2d7955c4d8c703f978
Marina Meyta <somekindofcookiemonster@gmail.com>
2014-12-16 18:58:53 +0300

    ICQ bug fixed

--- sources/plugins/TaskMessengers/src/XMLReader_ICQContacts.cpp
+++ sources/plugins/TaskMessengers/src/XMLReader_ICQContacts.cpp
@@ -13,14 +13,9 @@ bool XMLReader_ICQContacts::read(QString inputFile, QString outPath)
 {
 
     QFile file(inputFile);
-    //m_vContacts.clear();
-
-
     {
         QDir dir(outPath);
         dir.mkdir("icq");
-        //dir.mkdir("icq/messages");
-
     }
     writerMessagesPidgin icqContacts(outPath + "//icq/contacts.xml", "icq");
 
@@ -32,19 +27,14 @@ bool XMLReader_ICQContacts::read(QString inputFile, QString outPath)
             xml.readNext();
 			if (xml.qualifiedName() == "c")
 			{
-                //ICQContact tmp; // объявляем временный объект
                 QString mail, nick;
 				if(xml.attributes().value("email") != "")
-                    //tmp.setEmail(xml.attributes().value("email").toString());
                     mail = xml.attributes().value("email").toString();
-                    //qDebug()<< mail;
-				if(xml.attributes().value("nick") != "")
-                    //tmp.setNick(xml.attributes().value("nick").toString());
-                    nick = xml.attributes().value("nick").toString();
-                    //qDebug()<< nick;
-                 //m_vContacts.push_back(tmp);
-                icqContacts.writeContactList(mail, nick);
 
+				if(xml.attributes().value("nick") != "")
+                    nick = xml.attributes().value("nick").toString();
+
+                icqContacts.writeContactList(mail, nick);
 			}
         } while(!xml.atEnd());
         // закрываем файл если удалось его открыть
@@ -53,26 +43,4 @@ bool XMLReader_ICQContacts::read(QString inputFile, QString outPath)
     }
     return false;
 };
-// --------------------------------------------------------------------
 
-/*void XMLReader_ICQContacts::write(QString outputFile)
-{
-	QFile file(outputFile);
-    if(file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append)) 
-    {
-        QTextStream out(&file);
-
-		for(int i = 0; i < m_vContacts.size(); i++)
-		{
-			ICQContact tmp = m_vContacts[i];
-			if (!tmp.email().isEmpty() || !tmp.nick().isEmpty())
-			{
-				out << "email: " << tmp.email() << " nick: " << tmp.nick() << endl;
-				// временно дублируем ввывод в консоль
-				qDebug() << "email: " << tmp.email() << " nick: " << tmp.nick();
-			}
-		}
-        file.close();
-    };
-};*/
-// --------------------------------------------------------------------
