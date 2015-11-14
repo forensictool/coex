364a1099b1d1ee011a9d8bb8ad3737ae29b49679
Andrey <alaghal@mail.ru>
2015-11-14 18:11:42 +0600

    update #11: fix maintainer name

--- create_package.sh
+++ create_package.sh
@@ -26,13 +26,14 @@ cat bin/README >> debian/DEBIAN/copyrigth
 
 version=$(git describe --long)
 platform="all"
+maintainer="$(git config user.name)"
 echo "Package: $papackage_name" > debian/DEBIAN/control
 echo "Version: $version" >> debian/DEBIAN/control
 echo "Section: admin" >> debian/DEBIAN/control
 echo "Priority: optional" >> debian/DEBIAN/control
 echo "Architecture: $platform" >> debian/DEBIAN/control
 echo "Depends: g++ (>= 4.8.4)" >> debian/DEBIAN/control
-echo "Maintainer: Andrey Seryakov" >> debian/DEBIAN/control
+echo "Maintainer:1 $maintainer" >> debian/DEBIAN/control
 echo "Description: Project KIBEVS-1401: Computer forensics" >> debian/DEBIAN/control
 
 name="$papackage_name""_""$version""_""$platform.deb"
