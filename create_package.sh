87e45f82257cc2c9c6514104920f22a88a4af4c7
Igor Polyakov <fox.user.3@gmail.com>
2015-11-30 17:32:13 +0600

    add md5sum calc in script create_package & changelog show difference between last tag and current version

--- create_package.sh
+++ create_package.sh
@@ -19,11 +19,17 @@ mkdir -p debian/usr/share/man
 touch debian/usr/share/doc/$papackage_name/copyrigth
 touch debian/usr/share/man/
 
+## The md5sums file (DEBIAN/md5sums)
+# !! It works incorrectly
+#find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > debian/DEBIAN/md5sums
+
+## The changelog file, DEBIAN/changelog
 ./gen_change_log.sh > debian/DEBIAN/changelog
 
 cat bin/LICENSE > debian/DEBIAN/copyrigth
 cat bin/README >> debian/DEBIAN/copyrigth
 
+## The control file, DEBIAN/control
 version=$(git describe --long)
 platform="all"
 maintainer="$(git config user.name)"
@@ -33,7 +39,7 @@ echo "Section: admin" >> debian/DEBIAN/control
 echo "Priority: optional" >> debian/DEBIAN/control
 echo "Architecture: $platform" >> debian/DEBIAN/control
 echo "Depends: g++ (>= 4.8.4)" >> debian/DEBIAN/control
-echo "Maintainer:1 $maintainer" >> debian/DEBIAN/control
+echo "Maintainer: $maintainer" >> debian/DEBIAN/control
 echo "Description: Project KIBEVS-1401: Computer forensics" >> debian/DEBIAN/control
 
 name="$papackage_name""_""$version""_""$platform.deb"
