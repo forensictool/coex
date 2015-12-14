c936fca37f1c69712d926cd2bd8b44e6de7920b3
Igor Polyakov <fox.user.3@gmail.com>
2015-12-14 11:40:35 +0600

    update #11: fix print email in changelog

--- gen_change_log.sh
+++ gen_change_log.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 papackage_name="coex"
 version=$(git describe --long)
-maintainer="$(git config user.name)" 
+maintainer_name="$(git config user.name)"
+maintainer_email="$(git config user.email)"
 echo "|----------------------------------------------------------------------------------------|"
 echo "$  papackage_name ($version) ; urgency=low" 
 git tag -l | sort -u -r | while read TAG ; do
@@ -16,6 +17,6 @@ git tag -l | sort -u -r | while read TAG ; do
 done
 FIRST=$(git tag -l | head -1)
 #echo
-echo " -- $maintainer <alaghal@mail.ru> $(date -R) --"
+echo " -- $maintainer_name $maintainer_email $(date -R) --"
 echo "|----------------------------------------------------------------------------------------|"
 #GIT_PAGER=cat git log --no-merges --format=" * %s" $FIRST
