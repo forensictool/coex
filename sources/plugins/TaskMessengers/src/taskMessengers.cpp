8b3268d0ee05acc6d589c544a6e7b22f7fd6d2ab
MarinaMeyta <somekindofcookiemonster@gmail.com>
2016-03-24 15:28:31 +0600

    task messengers: searching XML added

--- sources/plugins/TaskMessengers/src/taskMessengers.cpp
+++ sources/plugins/TaskMessengers/src/taskMessengers.cpp
@@ -9,7 +9,8 @@
 #include <QXmlStreamWriter>
 #include <QTextStream>
 #include <QDebug>
-#include <QStringList>
+//#include <QStringList>
+#include <QDirIterator>
 
 TaskMessengers::TaskMessengers() : m_pbDebug(false)
 {
@@ -57,14 +58,29 @@ void TaskMessengers::init(const coex::IConfig *config)
 
 bool TaskMessengers::execute()
 {
-    if(m_pbDebug) {
+    if(m_pbDebug)
+    {
         qDebug() << "DEBUG MOD";
         qDebug() << QString("Input folder %1").arg(m_pConfig->inputFolder());
         qDebug() << QString("Output folder %1").arg(m_pConfig->outputFolder());
     };
 
-    XMLReader_ICQContacts reader; // создаем экземпляр класса
-    reader.read(m_pConfig->inputFolder() + "/Users/Default/AppData/Roaming/test.xml", m_pConfig->outputFolder());
+    QString dirStr(m_pConfig->inputFolder());
+    QString outDirStr(m_pConfig->outputFolder());
+    QDirIterator fileListDirit(dirStr, QDir::Files | QDir::NoSymLinks, QDirIterator::Subdirectories);
+
+    while(fileListDirit.hasNext())
+    {
+        QString str = QString("%1").arg(fileListDirit.next());
+        QFileInfo fInfo(str);
+        if(fInfo.suffix() == "xml")
+        {
+//            qDebug() << QString("fInfo.absoluteFilePath() = ").arg(fInfo.absoluteFilePath());
+            // парсинг XML-файла с контактами
+            XMLReader_ICQContacts reader; // создаем экземпляр класса
+            reader.read(fInfo.absoluteFilePath(), outDirStr);
+        }
+    }
 
     return true;
 }
